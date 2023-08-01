3c762407e9f3694b66cec5dcbbc08c0a967d1bb9
Connor Mulhall <43109407+conwayconstar@users.noreply.github.com>
2023-08-01 11:26:27 +0100

    fix: remove console logs

--- test/contracts/ERC1271.sol
+++ test/contracts/ERC1271.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "hardhat/console.sol";
 
 interface IERC1271 {
     function isValidSignature(bytes32 _hash, bytes memory _signature) external view returns (bytes4 magicValue);
